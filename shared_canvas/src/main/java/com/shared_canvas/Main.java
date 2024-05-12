689fe81ca192211be1c46e8e2b4582952b1638da
Chia-Hua-Yang <f74116306@gs.ncku.edu.tw>
2024-05-12 22:06:41 +0800

    hi

--- shared_canvas/src/main/java/com/shared_canvas/Main.java
+++ shared_canvas/src/main/java/com/shared_canvas/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This is Sean!");
         System.out.println("This is Eve!");
+        System.out.println("hi");
     }
 }
